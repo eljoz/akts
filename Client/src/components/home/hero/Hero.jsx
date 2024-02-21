8118737bc2edcd88463ccb0a4abe4c645fe2f482
eljoz <eljoz360@gmail.com>
2024-02-21 09:47:14 +0100

    final

--- Client/src/components/home/hero/Hero.jsx
+++ Client/src/components/home/hero/Hero.jsx
@@ -7,16 +7,16 @@ const Hero = () => {
     <>
       <section className='hero'>
         <div className='container'>
-          <Heading title='IT CAN ONLY GO FASTER ' subtitle='Get your package to its destination swiftly.' />
+          <Heading title='IT CAN ONLY GET FASTER ' subtitle='Get your package to its destination swiftly.' />
 
           <form className='flex'>
             <div className='box'>
-              <span>City/Street</span>
-              <input type='text' placeholder='Location' />
+              <span>State</span>
+              <input type='text' placeholder='State' />
             </div>
             <div className='box'>
-              <span>Location Type</span>
-              <input type='text' placeholder='Location Type' />
+              <span>City/Street</span>
+              <input type='text' placeholder='City' />
             </div>
             <div className='box'>
               <span>Price Range</span>
